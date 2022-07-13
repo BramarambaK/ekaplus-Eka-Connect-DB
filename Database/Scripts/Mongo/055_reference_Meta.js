a77d6b15ef1cb1c7cf762b682cab6603e3dd2fa2
BramarambaK <40611641+BramarambaK@users.noreply.github.com>
2022-07-13 17:59:20 +0530

    Delete Database/Scripts/Mongo directory

--- Database/Scripts/Mongo/055_reference_Meta.js
+++
@@ -1,6 +0,0 @@
-db.getCollection('reference_Meta').insert({ "type" : "property",     "name" : "eka_connect_host",     "propertyLevel" : "tenant,app,user",     "dataType" : "String",     "fieldtype" : "textbox",     "length" : "120" }
-    )//;
- 
---//@UNDO
-db.getCollection('reference_Meta').remove({ "type" : "property",     "name" : "eka_connect_host",     "propertyLevel" : "tenant,app,user"}
-    )//;
